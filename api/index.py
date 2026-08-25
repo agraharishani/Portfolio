98f9655ef98a6c23966d41102fdd926f1f079fce
agraharishani <agraharishani2006@gmail.com>
2026-08-25 22:44:55 +0530

    Refactor code structure for improved readability and maintainability

--- api/index.py
+++ api/index.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
-from app import app
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path="",
+            static_folder='public',
+            template_folder='templates'
+            )
 
 @app.route("/")
 def home():
