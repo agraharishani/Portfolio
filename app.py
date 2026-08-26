4f7afc6ed708abbded3d5b271bf400bfb3b64087
agraharishani <agraharishani2006@gmail.com>
2026-08-26 20:12:31 +0530

    Add initial HTML templates for home and projects pages, and configure Vercel deployment settings
    - Created home.html for the main landing page with navigation, hero section, skills, featured projects, and contact information.
    - Developed myprojects.html to showcase individual projects with details and technologies used.
    - Added vercel.json for deployment configuration to Vercel, specifying the Python build and routing.

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return  render_template("index.html")
+    return  render_template("home.html")
 
 @app.route("/about")
 def about():
     return  render_template("about.html")
 
-@app.route("/my-projects")
-def my_projects():
-    return  render_template("my-projects.html")
+@app.route("/myprojects")
+def myprojects():
+    return  render_template("myprojects.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
